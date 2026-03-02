57edfca76424928669de8b4b3c9af83a0217565e
Ralph Castilleja <ralphacastilleja@Ralphs-MacBook-Air.local>
2026-03-02 08:25:46 -0600

    COnditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,29 @@
 <template>
-    <div>
-      <div>
-        Press down the "Enter" key will trigger a console log print:
-        <input type="text" @keydown.enter="logKey('Enter')">
-      </div>
-  
-      <div>
-        Press down the "Arrow Down" key will trigger a console log print:
-        <input type="text" @keydown.down="logKey('Arrow Down')">
-      </div>
-  
-      <div>
-        Press down the "Space" key will trigger a console log print:
-        <input type="text" @keydown.space="logKey('Space')">
-      </div>
-  
-      <div>
-        Press down the "b" key will trigger a console log print:
-        <input type="text" @keydown.b="logKey('b')">
-      </div>
-
-      <div>
-        Press down the "ctrl + c " key will trigger a console log print:
-        <input type="text" @keydown.ctrl.c="logKey('ctrl + c')">
-      </div>
-
-    </div>
   <h1>{{ message }}</h1>
 
-  <button v-on:click="replaceText('v-on is fun!')">Replace text</button>
-  <button @click="replaceText('v-on is fun!')">Replace text</button>
-  <hr/>
-  <p> An input field where the user can only input numbers:</p>
-  <input type="text" @keydown="handleInput($event)">
+  <p>
+    What has keys but can't open locks, space but no room, and can't go outside? What am I?
+  </p>
+
+  <button style="width: 200px" @click="showAnswer = !showAnswer ">
+  {{ showAnswer ? "Hide Answer" : "Show Answer" }}</button>
+
+  <div v-if="showAnswer">A keyboard.</div>
+  <div v-show="showAnswer">A keyboard.</div>
 
 
 
-  
-<hr/>
-<div style ="width: 100px; height: 100px; background-color: lightblue" @contextmenu.prevent="console.log('Show a custom context menu')">
-  </div>
-  <div id="mouseover" @mouseover="fun1">
-    <textarea @mouseover.stop="fun2($event)">This is text area</textarea>"
-  </div>"
-
-
-  
+  <div v-if="Math.random() > 0.5">Now you see me .</div>
+  <div v-else>Now you don't.</div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-let message = ref('Hello, v-on!')
+let message = ref('Hello, v-if!')
 
-function replaceText(msg) {
-  message.value = msg
-}
-function handleInput(event) {
-  console.log(event)
-let keyCode = event.keyCode
-if (keyCode < 48 || keyCode > 57) {
-  event.preventDefault()
-  }
-}
-
-function fun1() {
-  console.log('Mouse is over the div')
-}
-
-function fun2(event) {
-  //event.stopPropagation()
-  console.log('Mouse is over the textarea')
-}
-
-
-
+let showAnswer = ref(false)
 </script>
 
-
 <style scoped>
-#mouseover {
-  margin: 100px;
-  text-align: right;
-  background-color: purple;
-  width: 300px;
-  height: 300px;
-}
-
-
-
 </style>
