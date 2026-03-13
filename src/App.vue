dd3961d071130e6fd54dc797eecf7bfd908ad55a
Ralph Castilleja <ralphacastilleja@Ralphs-MacBook-Air.local>
2026-03-13 13:22:49 -0500

    Vue comp intro

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,24 @@
-<template>
-  <h1>{{ message }}</h1>
-  <button @click="sortUsersByAge">sort users by age</button>
-  <ul>
-  <li v-for="(user,index) in users" :key="user.id">
-    {{ index }} - {{ user.id }} - {{ user.name }} - {{ user.age }} - 
-    {{ user.isActive }}
-  </li>
-</ul>
+    <template>
+    
+    
 
+  <ButtonCounter></ButtonCounter>
+  <ButtonCounter></ButtonCounter>
 
-  <table>
-    <tr>
-      <th>Index</th>
-      <th>Id</th>
-      <th>Name</th>
-      <th>Age</th>
-      <th>Operation</th>
-    </tr>
+  <button-counter></button-counter>
 
-    <tr v-for="(user, index) in users" :key="user.id">
-      <td>{{ index }}</td>
-      <td>{{ user.id }}</td>
-      <td :class = "{inactive: !user.isActive}">{{ user.name }}</td>
-      <td>{{ user.age }}</td>
-      <td>
-        <button @click="user.isActive = !user.isActive">
-          {{ user.isActive ? 'Deactivate' : 'Restore' }}
-        </button>
-      </td>
-    </tr>
-    <hr/>
-    <div v-for="(value, key,index) in users[0]" > {{}}-{{key }}: {{ value }}</div>
-  </table>
+  <hr />
 
+  <div style="background-color: orange; padding: 5px">
+    This is the root component.
+    <ComponentA></ComponentA>
+    <ComponentA></ComponentA>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-let message = ref('Hello, v-for!')
-
-const users = ref([
-  { id: 1001, name: 'John Smith', age: 26, isActive: false },
-  { id: 1002, name: 'Tom Doe', age: 16, isActive: false },
-  { id: 1003, name: 'Frankin Wong', age: 18, isActive: true }
-])
-
-function sortUsersByAge() {
-  users.value.sort((a, b) => a.age - b.age)
-}
+import ButtonCounter from './ButtonCounter.vue'
+import ComponentA from '@/ComponentA.vue'
 </script>
 
-<style scoped>
-.inactive{
-  color: red;
-  text-decoration: line-through;
-}
-</style>
+<style scoped></style>
