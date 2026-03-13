ffdefcd622de075bc4e20ab61da7587a5a79cc21
Ralph Castilleja <ralphacastilleja@Ralphs-MacBook-Air.local>
2026-03-13 13:59:31 -0500

    new template-refs

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="card">
+    <h1 ref="title">Hello, Template Refs!</h1>
+    <input type="text" ref="input" />
+    <button @click="printDomElements">Print DOM elements in console log</button>
+    <button @click="changeTitle">Change title</button>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { onMounted, ref } from 'vue'
+
+let title = ref()
+const input = ref()
+
+function printDomElements() {
+  console.log(title.value)
+  console.log(input.value)
+}
+
+function changeTitle() {
+  title.value.innerText = 'Hello world!'
+}
+
+onMounted(() => {
+  input.value.focus()
+})
+</script>
+
+<style scoped>
+.card {
+  background-color: purple;
+  color: white;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+}
+</style>
